967f31ddd6d69d01a92b74245f39dc45d8f1007d
r-antonio16 <47504810+r-antonio16@users.noreply.github.com>
2023-10-26 09:21:11 +0100

    closes #34 - Delete Scraper Template related classes

--- src/main/java/pt/zerodseis/services/webscraper/processors/WebContentProcessor.java
+++
@@ -1,8 +0,0 @@
-package pt.zerodseis.services.webscraper.processors;
-
-import pt.zerodseis.services.webscraper.templates.WebScraperTemplate;
-
-public interface WebContentProcessor {
-
-    WebScraperTemplate generateTemplate();
-}
--- src/main/java/pt/zerodseis/services/webscraper/templates/WebScraperTemplate.java
+++
@@ -1,9 +0,0 @@
-package pt.zerodseis.services.webscraper.templates;
-
-import java.io.Serializable;
-
-public interface WebScraperTemplate extends Serializable {
-
-    String getWebSite();
-
-}
